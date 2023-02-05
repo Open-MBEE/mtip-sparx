842d2962822927721e2ab4b05c8785873f478074
Daniel Siegl <daniel.siegl@lieberlieber.com>
2023-02-05 15:21:18 +0100

    * Added "unified" ID export to the code. * Simplified the creation of the ID's and moved it to a centralized location.

---
+++ MTIP/Translations/MTIPCommon.cs
@@ -0,0 +1,41 @@
+﻿using MTIP.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MTIP.Translations
+{
+    internal static class MTIPCommon
+    {
+        //DS: Not the way I would do this.
+        public static AttributeConstants attributeConstants =new AttributeConstants();
+        public static RelationshipConstants relationshipConstants = new RelationshipConstants();
+        public static HUDSConstants hudsConstants =new HUDSConstants();
+
+        internal static XmlElement CreateIdElement(XmlDocument xmlDocument, string eaElementGuid)
+        {
+
+            XmlElement idElement = xmlDocument.CreateElement(attributeConstants.id);
+            idElement.SetAttribute(hudsConstants.dtype, hudsConstants.dict);
+
+            //unified ID shall be the new Vendor independed ID field for cross tool roundtrips.
+            XmlElement unifiedIdElement = xmlDocument.CreateElement(hudsConstants.unified);
+            unifiedIdElement.SetAttribute(hudsConstants.dtype, hudsConstants.str);
+            unifiedIdElement.InnerText = eaElementGuid.ToString();
+
+            idElement.AppendChild(unifiedIdElement);
+
+            //unified ID shall be the new Vendor specific ID field for cross tool roundtrips, best case this is not required going forward
+            XmlElement eaIdElement = xmlDocument.CreateElement(hudsConstants.ea);
+            eaIdElement.SetAttribute(hudsConstants.dtype, hudsConstants.str);
+            eaIdElement.InnerText = eaElementGuid.ToString();
+
+            idElement.AppendChild(eaIdElement);
+
+            return idElement;
+        }
+    }
+}
